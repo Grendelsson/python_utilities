cd64d14a39ef6fa988cb92d7a1e9ea371337119c
Grendelsson <Grendelsson@users.noreply.github.com>
2022-05-05 11:32:23 +0100

    Create setup file
    Not sure I know how this works, but we'll see?

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ga4_to_df',
+      version='1.0',
+      description='Utility to pull GA4 report as dataframe',
+      author='Grendelsson',
+      author_email='none',
+      url='https://github.com/Grendelsson/python_utilities/',
+      packages=['ga4_to_df'],
+     )
